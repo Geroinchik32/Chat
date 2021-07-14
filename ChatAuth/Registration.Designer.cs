21a63c75ad7254433058dc8606573302acaed2e5
Geroinchik32 <andrey.geraskin00@yandex.ru>
2021-07-14 11:43:31 +0300

    111

--- ChatAuth/Registration.Designer.cs
+++ ChatAuth/Registration.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace ChatAuth
+﻿// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+namespace ChatAuth
 {
     partial class Registration
     {
